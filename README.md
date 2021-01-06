2278509b78b019072303eb442d5fece61729577b
jwnukoski <jwnukoski@protonmail.com>
2021-01-06 01:34:33 -0500

    - Eslint - ReadMe - Private repo cloning

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # github-dl
 Unofficial app to download all your repos locally.
 
-# Features
-Currently only supports cloning public respositories.
+# Current Features
+- Clone publicly respositories with no token
+- Clone privately owned repositories with a token
+
+# Planned Features
+- Cloning organization repos
 
 # Running
 - npm install
 - npm run start
 - Repos will be cloned into ./users/[username]
+- If you are cloning private repos create a private token (https://github.com/settings/tokens) with private repo access, and store it in ./users/[username]/token.txt
+
+# Known issues
+- Token is stored in a file in plain-text (BAD). Prompt added to delete file after.
+- You must respond as 'no' to the first prompt, if you want to be asked to clone private repos later
+- Question chain needs changed to promise chain or async await
+- Some public repos aren't being clone in the private prompt, which are in the public prompt
--- index.js
+++ index.js
@@ -1,37 +1,103 @@
-const fs = require('fs');
-const axios = require('axios');
-const readline = require('readline');
-const shell = require('shelljs');
+(function () {
+  const fs = require('fs')
+  const axios = require('axios')
+  const readline = require('readline')
+  const shell = require('shelljs')
 
-function getPublicRepos(username) {
-  axios.get(`http://api.github.com/users/${username}/repos`).then(res => {
-  return repos = res.data.map(row => {
-      return {url: row.html_url, name: row.name};
-    });
-  }).then(repos => {
-    if (repos.length <= 0)
-      throw `No repos found for ${username}`;
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
-    if (!fs.existsSync(`./users/${username}`))
-        fs.mkdirSync(`./users/${username}`);
+  function makeUserDir (username) {
+    if (!fs.existsSync(`./users/${username}`)) { fs.mkdirSync(`./users/${username}`) }
+  }
 
-    repos.forEach(val => {
-      shell.exec(`git clone ${val.url} ./users/${username}/${val.name}`)
-    });
+  function promptYesNo (question, callback) {
+    rl.question(`${question} [y/n]: `, (input) => {
+      input = input.toLowerCase()
 
-    return 'Done';
-  }).then((res) => {
-    console.log(`${res}`);
-  }).catch(err => {
-    console.log(`Error: ${err}`)
-  });
-}
+      let status = true
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+      if (input !== 'y') { status = false }
 
-rl.question('Input Github username: ', (user) => {
-  getPublicRepos(user);
-});
+      callback(status)
+    })
+  }
+
+  function getPubliclyOwnedRepos (username) {
+    return axios.get(`http://api.github.com/users/${username}/repos`).then(res => {
+      return res.data.map(row => {
+        return { url: row.html_url, name: row.name }
+      })
+    }).then(repos => {
+      if (repos.length <= 0) { throw new Error(`No repos found for ${username}`) }
+
+      makeUserDir()
+
+      repos.forEach(val => {
+        shell.exec(`git clone ${val.url} ./users/${username}/${val.name}`)
+      })
+
+      return 'Done cloning public repos.'
+    }).then((res) => {
+      console.log(`${res}`)
+    }).catch(err => {
+      console.error(`Error: ${err}`)
+    })
+  }
+
+  function getPrivatelyAndPubliclyOwnedRepos (username) {
+    makeUserDir()
+    console.log(`\nIn order to access private repositories, you will need to provide a personal access token with private repo access.\nYou can create one here: https://github.com/settings/tokens\nStore it in ./users/${username}/token.txt\nDelete this when you're done!!!\n`)
+
+    promptYesNo('Continue? ', choice => {
+      if (!choice) { return }
+
+      fs.readFile(`./users/${username}/token.txt`, 'utf8', (err, token) => {
+        if (err) {
+          console.error(`Error reading ./users/${username}/token.txt:\nerr`)
+          return
+        }
+
+        return axios.get(`https://api.github.com/search/repositories?q=user:${username}`, {
+          headers: {
+            Authorization: `token ${token}`
+          }
+        }).then(res => {
+          return res.data.items.map(row => {
+            return { url: row.html_url, name: row.name }
+          })
+        }).then(repos => {
+          return repos.forEach(val => {
+            shell.exec(`git clone https://${username}:${token}@github.com/${username}/${val.name}.git ./users/${username}/${val.name}`)
+          })
+        }).then(() => {
+          promptYesNo(`Done cloning public and private repos for ${username}\nDo you want to delete the token file?`, choice => {
+            if (choice) {
+              fs.unlink(`./users/${username}/token.txt`, () => {
+                console.log('Token removed!')
+              })
+            }
+            console.log('Done cloning privately owned repos.')
+          })
+        }).catch(err => {
+          console.error(`Error getting private repository: ${err}`)
+        })
+      })
+    })
+  }
+
+  rl.question('Input Github username: ', (username) => {
+    // TODO: Needs redone with proper promise chains or awaits
+    promptYesNo('Do you want to clone public repos?', choice => {
+      if (choice) {
+        getPubliclyOwnedRepos(username)
+      } else {
+        promptYesNo('Do you want to get private repos?', choice => {
+          if (choice) { getPrivatelyAndPubliclyOwnedRepos(username) }
+        })
+      }
+    })
+  })
+})()
